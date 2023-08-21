8c7211a7e59a2e1b28f45c88f002ebec8954d81e
jprodhon <62745781+jprodhon@users.noreply.github.com>
2023-08-21 23:15:35 +0200

    Update your_db_id.py

--- your_db_id.py
+++ your_db_id.py
@@ -1,8 +1,8 @@
-notion_token = "secret_Dml93qlm4qOMYtvzkmZjh9Tq03wQqeuBoo1tZQyiuP2"
+notion_token = "secret_Dml93qlm4qOM12er98Zjh9Tq03wQqeueee1tZQyrer2"
 
-page_id = "86169741315d4d23b30ba11b922a528f"
-calendar_db_id = "0adf117a28ce4d85bbcf597b3fc276c7"
-week_db_id = "96d0e462f65445a7a3f27b88fffddbe0"
+page_id = "86154141345d4d23b22ba11b932a527f"
+calendar_db_id = "0adf147a12ce4d85bbcf555b3fc333c3"
+week_db_id = "45d0e462f67788a7a3f27b66frrddbe0"
 
 sport_list = ["Swim","Bike","Run"]
 sport_list_ext = sport_list + ["All"]
